6046ad25ebf421366f849658babe8b38cb8d16a4
Claire Furney <infuerno@fastmail.fm>
2018-12-04 22:22:12 +0000

    Update path for 'Learn more' url used in notification

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+# NOTE - see log messages using
+# log show --predicate 'process == "logger"' --last 30m
+
 PICTURE_DIR="$HOME/Dropbox/Pictures/Wallpaper/Bing"
 ICON_DIR="$HOME/Projects/GitHub/bing-wallpaper/Icons"
 
-logger -s "Checking for new Bing Wallpaper..."
+logger -s "Checking for new Bing wallpaper ..."
 
 data=$(curl -sL http://www.bing.com/?cc=gb)
 
@@ -11,31 +15,32 @@ urls=( $( echo $data | \
     sed -e "s/url: \"\([^\"]*\)\".*/http:\/\/bing.com\1/" | \
     sed -e "s/\\\//g") )
 
-logger -s "Url of current wallpaper(s): $urls"
+logger -s "Url of today's Bing wallpaper: $urls"
 
 for p in ${urls[@]}; do
     filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
     if [ ! -f $PICTURE_DIR/$filename ]; then
-        logger -s "Downloading: $filename ..."
+        logger -s "Downloading to $PICTURE_DIR/$filename ..."
         curl -Lo "$PICTURE_DIR/$filename" $p
     else
-        logger -s "Already downloaded, skipping downloading: $filename ..."
+        logger -s "Already downloaded to $PICTURE_DIR/$filename"
     fi
     logger -s "Setting desktop wallpaper to $PICTURE_DIR/$filename"
     osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$PICTURE_DIR/$filename\""
 done
 
-logger -s "Extracting notification info..."
+logger -s "Extracting notification info ..."
 
 # extract notification message
 message=$(echo $data | grep -Eo "mcImgData ={\"copyright\":\"[^\"]*\"" | \
     sed -e "s/mcImgData ={\"copyright\":\"\([^\"]*\)\".*/\1/")
 
 # extract url
-url=$(echo $data | grep -Eo -m 1 "\"/search.q=[^\"]*\"" | \
-    head -1 | \
-    sed -e "s/\"\([^\"]*\)\"/http:\/\/www.bing.com\1/" | \
+url=$(echo $data | grep -Eo -m 1 "href=\"/search.q=[^\"]*\"" | \
+	head -1 | \
+    sed -e "s/href=\"\([^\"]*\)\"/https:\/\/www.bing.com\1/" | \
     sed -e "s/&amp;/\&/g")
+logger -s "$url2"
 
 # unencode url
 #logger -s "Encoding url: $url"
@@ -43,5 +48,3 @@ url=$(echo $data | grep -Eo -m 1 "\"/search.q=[^\"]*\"" | \
 
 logger -s "Creating notification: $message at $url"
 /usr/local/bin/terminal-notifier -title "Bing Wallpaper" -message "$message" -open "$url" -appIcon "$ICON_DIR/bing-icon.png"
-
-
