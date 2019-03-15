84ede630846d8ee0541ad855593486d5ec09bb17
Claire Furney <infuerno@fastmail.fm>
2019-03-15 23:07:33 +0000

    Remove extra logging / comments

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -8,9 +8,7 @@ ICON_DIR="$HOME/Projects/GitHub/bing-wallpaper/Icons"
 BASE_URL="https://www.bing.com"
 
 logger -s "Checking for new Bing wallpaper ..."
-
 data=$(curl -s "$BASE_URL/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB")
-logger -s "Data for today's image is $data"
 
 image_url=$( echo $data | grep -Eo '"url":.*?[^\\]"' | awk -F'"' '{print $4}' )
 logger -s "Url of today's Bing wallpaper: $BASE_URL$image_url"
@@ -21,20 +19,18 @@ logger -s "Using filename: $filename"
 if [ ! -f $PICTURE_DIR/$filename ]; then
     logger -s "Downloading to $PICTURE_DIR/$filename ..."
     curl -Lo "$PICTURE_DIR/$filename" $BASE_URL$image_url
+
     # Remove previous picture from Today folder
     rm -f "$PICTURE_DIR"/Today/*
     # Copy newly downloaded file to Today folder
     cp "$PICTURE_DIR/$filename" "$PICTURE_DIR/Today/$filename"
+
     logger -s "Setting desktop wallpaper to $PICTURE_DIR/$filename"
     osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$PICTURE_DIR/$filename\""
 
     logger -s "Extracting notification info ..."
-
-    # extract notification message
     copyright=$( echo $data | grep -Eo '"copyright":.*?[^\\]"' | awk -F'"' '{print $4}' )
     logger -s "$copyright"
-
-    # extract url
     copyrightlink=$( echo $data | grep -Eo '"copyrightlink":.*?[^\\]"' | awk -F'"' '{print $4}' )
     logger -s "$copyrightlink"
 
