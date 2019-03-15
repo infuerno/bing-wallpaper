f3fa0bf6a4666f26d62f45e8aaada71875af1220
Claire Furney <infuerno@fastmail.fm>
2019-03-15 23:03:47 +0000

    Update to use JSON api

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,55 +1,46 @@
 #!/usr/bin/env bash
 
-# NOTE - see log messages using
+# NOTE - display log messages using
 # log show --predicate 'process == "logger"' --last 30m
 
 PICTURE_DIR="$HOME/Dropbox/Pictures/Wallpaper/Bing"
 ICON_DIR="$HOME/Projects/GitHub/bing-wallpaper/Icons"
+BASE_URL="https://www.bing.com"
 
 logger -s "Checking for new Bing wallpaper ..."
 
-data=$(curl -sL https://www.bing.com/?cc=gb)
+data=$(curl -s "$BASE_URL/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB")
+logger -s "Data for today's image is $data"
 
-urls=( $( echo $data | \
-    grep -Eo "url: ?\".*?\"" | \
-    sed -e "s/url: \"\([^\"]*\)\".*/https:\/\/bing.com\1/" | \
-    sed -e "s/\\\//g") )
+image_url=$( echo $data | grep -Eo '"url":.*?[^\\]"' | awk -F'"' '{print $4}' )
+logger -s "Url of today's Bing wallpaper: $BASE_URL$image_url"
 
-logger -s "Url of today's Bing wallpaper: $urls"
+filename=$(echo $image_url | sed -e "s/[^.]*\.\([^.]*\).*/\1.jpg/")
+logger -s "Using filename: $filename"
 
-for p in ${urls[@]}; do
-    filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
-    if [ ! -f $PICTURE_DIR/$filename ]; then
-        logger -s "Downloading to $PICTURE_DIR/$filename ..."
-        curl -Lo "$PICTURE_DIR/$filename" $p
-        # Remove previous picture from Today folder
-        rm -f "$PICTURE_DIR"/Today/*
-        # Copy newly downloaded file to Today folder
-        cp "$PICTURE_DIR/$filename" "$PICTURE_DIR/Today/$filename"
-        logger -s "Setting desktop wallpaper to $PICTURE_DIR/$filename"
-        osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$PICTURE_DIR/$filename\""
+if [ ! -f $PICTURE_DIR/$filename ]; then
+    logger -s "Downloading to $PICTURE_DIR/$filename ..."
+    curl -Lo "$PICTURE_DIR/$filename" $BASE_URL$image_url
+    # Remove previous picture from Today folder
+    rm -f "$PICTURE_DIR"/Today/*
+    # Copy newly downloaded file to Today folder
+    cp "$PICTURE_DIR/$filename" "$PICTURE_DIR/Today/$filename"
+    logger -s "Setting desktop wallpaper to $PICTURE_DIR/$filename"
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$PICTURE_DIR/$filename\""
 
-        logger -s "Extracting notification info ..."
+    logger -s "Extracting notification info ..."
 
-        # extract notification message
-        message=$(echo $data | grep -Eo "mcImgData ={\"copyright\":\"[^\"]*\"" | \
-            sed -e "s/mcImgData ={\"copyright\":\"\([^\"]*\)\".*/\1/")
+    # extract notification message
+    copyright=$( echo $data | grep -Eo '"copyright":.*?[^\\]"' | awk -F'"' '{print $4}' )
+    logger -s "$copyright"
 
-        # extract url
-        url=$(echo $data | grep -Eo -m 1 "href=\"/search.q=[^\"]*\"" | \
-        	head -1 | \
-            sed -e "s/href=\"\([^\"]*\)\"/https:\/\/www.bing.com\1/" | \
-            sed -e "s/&amp;/\&/g")
-        logger -s "$url2"
+    # extract url
+    copyrightlink=$( echo $data | grep -Eo '"copyrightlink":.*?[^\\]"' | awk -F'"' '{print $4}' )
+    logger -s "$copyrightlink"
 
-        # unencode url
-        #logger -s "Encoding url: $url"
-        #url=$(python -c 'import sys, urllib; print urllib.unquote(sys.argv[1])' "$url")
-
-        logger -s "Creating notification: $message at $url"
-        /usr/local/bin/terminal-notifier -title "Bing Wallpaper" -message "$message" -open "$url" -appIcon "$ICON_DIR/bing-icon.png"
-    else
-        logger -s "Already downloaded to $PICTURE_DIR/$filename"
-        # TODO also check desktop is using this and set if not
-    fi
-done
+    logger -s "Creating notification: $copyright at $copyrightlink"
+    /usr/local/bin/terminal-notifier -title "Bing Wallpaper" -message "$copyright" -open "$copyrightlink" -appIcon "$ICON_DIR/bing-icon.png"
+else
+    logger -s "Already downloaded to $PICTURE_DIR/$filename"
+    # TODO also check desktop is using this and set if not
+fi
